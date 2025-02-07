faec5a67a3ffde597c4ba7b9cb78fb886b01e2a1
ArthurJarriau <146826979+ArthurJarriau@users.noreply.github.com>
2025-02-07 17:12:53 +0100

    Ticket 5

---
+++ app/admin/courses/[courseId]/course.query.ts
@@ -0,0 +1,49 @@
+import { prisma } from "@/lib/prisma";
+
+export const getCourse = async (courseId: string, userId: string,page: number) => {
+    const courses = await prisma.course.findUnique({
+        where: {
+            creatorId: userId,
+            id: courseId
+        },
+        select: {
+            id: true,
+            name: true,
+            image: true,
+            presentation: true,
+            users: {
+                take: 5,
+                skip: Math.max(0, page * 5),
+                select: {
+                    canceledAt: true,
+                    id: true,
+                    user: {
+                        select: {
+                            id: true,
+                            email: true,
+                            name: true
+                        }
+                }
+            }
+            },
+            _count: {
+                select:{
+                    lessons: true,
+                    users: true
+                }
+            }
+        }
+    });
+    
+    const users = courses?.users.map((user) => {
+        return {
+            canceled : user.canceledAt ? true : false,
+            ...user.user,
+        };
+    });
+    return {
+        ...courses,
+        users,
+        
+    };
+}
--- app/admin/courses/[courseId]/page.tsx
+++ app/admin/courses/[courseId]/page.tsx
@@ -1,27 +1,87 @@
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { getAuthSession } from "@/lib/auth";
 import { PrismaClient } from "@prisma/client";
-import Image from "next/image";
+import { getCourse } from "./course.query";
+import { Layout, LayoutContent, LayoutHeader, LayoutTitle } from "@/components/layout/layout";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Typography } from "@/components/ui/typography";
+import Link from "next/link";
+import { CoursePaginationButton } from "@/features/pagination/PaginationButton";
+import { buttonVariants } from "@/components/ui/button";
 
-export default async function Page({params}: {params: {courseId: string}}) {
+export default async function Page({params,searchParams}: {params: {courseId: string}, searchParams: Promise<{ [key: string]: string | string[] | undefined }>}) {
     const slug = params.courseId;
-    // console.log(slug);
+    const page = Number(searchParams.page ?? 1); 
     const courseId = slug;
-    // console.log(courseId);
+    const session = await getAuthSession();
     const prisma = new PrismaClient();
-    const course = await prisma.course.findUnique({
-        where: {
-            id: courseId
-        }
-    });
+    
+    const course = await getCourse(courseId, session.user.id,page);
+    console.log(course);
     return (
-        <Card>
+        <Layout>
+      <LayoutHeader>
+        <LayoutTitle>Courses</LayoutTitle>
+      </LayoutHeader>
+      <LayoutContent className="flex gap-4 flex-col md:flex-row">
+        <Card className="flex-[2]">
             <CardHeader>
-                <CardTitle>
-                    {course.name}
-                </CardTitle>
-                <Image src={course.image} alt={course.title} width={200} height={200}/>
-                
+                <CardTitle>Users</CardTitle>
             </CardHeader>
+          <CardContent>
+            <Table>
+            <TableHeader>
+                <TableRow> 
+                    <TableHead>Image</TableHead>
+                    <TableHead>Name</TableHead>
+                </TableRow>
+                </TableHeader>
+              <TableBody>
+                {course?.users?.map((user) => (
+                  <TableRow>
+                    <TableCell>
+                      <Avatar className="rounded">
+                        <AvatarFallback>{user.email?.[0]}</AvatarFallback>
+                        {user.image && (
+                          <AvatarImage src={user.image} alt={user.name ?? ''} />
+                        )}
+                      </Avatar>
+                    </TableCell>
+                    <TableCell>
+                      <Typography
+                        as={Link}
+                        variant="large"
+                        href={`/admin/users/${user.id}`}
+                      >
+                        {user.email}
+                      </Typography>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+            <CoursePaginationButton totalPage={course._count?.users ?? 0 / 5} page={page} baseUrl={`/admin/courses/${course.id}`} />
+          </CardContent>
         </Card>
+        <Card className="flex-1">
+            <CardHeader className="flex-row items-center gap-4 space-x-0">
+                    <Avatar className="rounded">
+                        <AvatarFallback>{course.name?.[0]}</AvatarFallback>
+                        {course.image && (
+                          <AvatarImage src={course.image} alt={course.name ?? ''} />
+                        )}
+                      </Avatar>
+                    <CardTitle>{course.name}</CardTitle>
+            </CardHeader>
+            <CardContent className="flex flex-col gap-1">
+                <Typography>{course._count?.users} users</Typography>
+                <Typography>{course._count?.lessons} lessons</Typography>
+                <Link className={buttonVariants({variant: "outline"})} href={`/admin/courses/${course.id}/edit`}>Edit</Link>
+                <Link className={buttonVariants({variant: "outline"})} href={`/admin/courses/${course.id}/lessons`}>Edit lessons</Link>
+            </CardContent>
+        </Card>
+      </LayoutContent>
+    </Layout>
     )
 }
